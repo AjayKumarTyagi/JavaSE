d6783ab353ed3e28edc1d2643bb2aa94bd7121f1
Ajay Tyagi <ajaytyagi0209@gmail.com>
2019-02-04 21:00:08 +0530

    third code added

---
+++ CharDemo.java
@@ -0,0 +1,13 @@
+class CharDemo
+{
+public static void main(String args[])
+{
+char ch;
+ch='G';
+System.out.println("ch= "+ch);
+ch++;
+System.out.println("ch= "+ch);
+ch=96;
+System.out.println("ch= "+ch);
+}
+}
