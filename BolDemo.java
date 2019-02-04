c4d0c2eda5709ec3165cbd900f42e52f719405df
Ajay Tyagi <ajaytyagi0209@gmail.com>
2019-02-04 21:00:50 +0530

    boolen use

---
+++ BolDemo.java
@@ -0,0 +1,16 @@
+class BolDemo
+{
+public static void main(String args[])
+{
+boolean a,b;
+a=true;
+b= false;
+System.out.println("b= "+b);
+System.out.println("a= "+a);
+if(a)
+System.out.println("executed");
+if(b)
+System.out.println("not executed");
+System.out.println("99<9 is"+(99<9));
+}
+}
