6671d8c26cf8b92c53c26bc89c9cb4e0beb82eb8
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2020-01-17 11:14:07 +0530

    adding cmd line arguments

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class Add
+{
+public static void main(String args[])
+{
+int a,b,c;
+a=Integer.parseInt(args[0]);  // converting 1 cmd line argument string into int
+b=Integer.parseInt(args[1]);  // converting 2 cmd line argument string into int
+c=a+b;
+System.out.println("result= "+c);
+}
+}
