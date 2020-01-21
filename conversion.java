265aae94bb995475d39c017cb221dfbf81d893af
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2020-01-21 18:56:50 +0530

    Added pounds to kilos conversion

---
+++ conversion.java
@@ -0,0 +1,8 @@
+class conversion
+{
+	public static void main(String args[]){
+		int pounds = 200;
+		double kilo = pounds * 0.45359237;
+		System.out.println(pounds+" pounds = "+kilo+" kilos");
+	}
+}
