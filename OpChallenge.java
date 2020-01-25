d503dd80fa59659280eafef5263af427916c2ff8
Ajay Kumar Tyagi <ajaytyagi0209@gmail.com>
2020-01-25 22:27:52 +0530

    comments added

--- OpChallenge.java
+++ OpChallenge.java
@@ -1,18 +1,18 @@
 class OpChallenge{
 	public static void main(String args[]){
-		double var1=20.00;
-		double var2=80.00;
+		double var1=20.00; // declaring var1
+		double var2=80.00; // declaring var2
 		double var3=(var1+var2)*100.00;
-		System.out.println("total= "+var3);
+		System.out.println("total= "+var3); // printing var3
 		double rem=var3%40.00;
-		System.out.println("remainder= "+rem);
-		boolean var4;
+		System.out.println("remainder= "+rem); // printing remainder
+		boolean var4; // declaring boolean variable
 		if(rem==0)
 			var4=true;
 		else
 			var4=false;
-		System.out.println("boolean var= "+var4);
+		System.out.println("boolean var= "+var4); // printing var4
 		if(var4!=true)
-			System.out.println("Got some remainder");
+			System.out.println("Got some remainder"); // this should not print
 	}
 }
