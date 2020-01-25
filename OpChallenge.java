d04807ee95ed43744ef5235621934c3fbd4f4692
Ajay Kumar Tyagi <ajaytyagi0209@gmail.com>
2020-01-25 22:30:21 +0530

    Operator challenge completed, hurray

--- OpChallenge.java
+++ OpChallenge.java
@@ -1,3 +1,51 @@
+/* Operator Challenge
+
+Step number one is to create a double variable
+
+with a value of 20.00.
+
+Step two create a second variable of type double
+
+with a value 80.00.
+
+Step three is to add both numbers together
+
+then multiply by 100.00.
+
+Step 4 use the remainder operator
+
+to figure out what the remainder
+
+from the result of the operation in step three and 40.00 is.
+
+Now we have used the modulus
+
+or remainder operator on ints in this course
+
+but I'm confirming here
+
+you can also do the same for double
+
+a variable of type double.
+
+Step 5 create a boolean variable
+
+that assigns the value true
+
+if the remainder in step four is 0.00
+
+or false if it's not zero.
+
+Step 6 output the boolean variable
+
+just to see what the result is
+
+and step seven write an if-then statement
+
+that displays a message 'got some remainder'
+
+if the boolean in step five is not true. */
+
 class OpChallenge{
 	public static void main(String args[]){
 		double var1=20.00; // declaring var1
