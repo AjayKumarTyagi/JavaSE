9d0d616c83b88e0d4d01fd8e8bfca2d174ed7b0d
Ajay Kumar Tyagi <ajaytyagi0209@gmail.com>
2020-02-12 15:06:11 +0530

    java list arrays added

---
+++ ListArray.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ListArray{
+  public static void main(String args[]){
+    List list = new ArrayList();
+    list.add(2);
+    list.add(5);
+    list.add(7);
+    list.remove(1);
+    list.add("ajmer");
+    list.add("Bhopal");
+    list.add("Chennai");
+    System.out.println(list);
+  }
+}
