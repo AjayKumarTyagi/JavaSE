4501fd6dcb9e3db1e41dfd57e55493d9a9cb2970
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2020-02-21 10:31:52 +0530

    Worker class added

---
+++ WorkerSalary.java
@@ -0,0 +1,26 @@
+class Worker{
+private String name;
+private double salary_rate;
+Worker(String name, double s_rate)
+{
+this.name=name;
+this.salary_rate=s_rate;
+}
+public void ComPay()
+{
+int hours=40;
+double salary=hours*this.salary_rate;
+System.out.println("name= "+this.name);
+System.out.println("rates= "+this.salary_rate);
+System.out.println("hours= "+hours);
+System.out.println("salary= "+salary);
+}
+public void ComPay(int days)
+{
+double salary=days*this.salary_rate;
+System.out.println("name= "+this.name);
+System.out.println("rates= "+this.salary_rate);
+System.out.println("days= "+days);
+System.out.println("salary= "+salary);
+}
+}
