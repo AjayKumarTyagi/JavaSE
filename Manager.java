ead5017b09be70d976ab21a7f7ec596488332a3d
Ajay Kumar Tyagi <ajaytyagi0209@gmail.com>
2020-02-23 18:57:29 +0530

    Employee class created

---
+++ Manager.java
@@ -0,0 +1,20 @@
+class Employee
+{
+  String name;
+  int empid;
+  double salary;
+  Employee(String name,int empid.double salary)
+  {
+    this.name=name;
+    this.empid=empid;
+    this.salary=salary;
+  }
+  public String DisplayName()
+  {
+    return this.name;
+  }
+  public double DisplaySalary()
+  {
+    return this.salary;
+  }
+}
