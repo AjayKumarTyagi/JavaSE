e39d7a8e13116f2d1a0221d0625a77a14ad56a65
Ajay Kumar Tyagi <ajaytyagi0209@gmail.com>
2020-03-25 11:40:40 +0530

    user defined packages added

---
+++ mypackage/Account.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class Account
+{
+  public double Display_Balance(double amt,double with)
+  {
+    return amt-with;
+  }
+}
---
+++ mypackage/IsEligible.java
@@ -0,0 +1,12 @@
+package mypackage;
+
+public class IsEligible
+{
+  public void getName(int age)
+  {
+    if(age<18)
+    System.out.println("Not eligible to vote");
+    else
+    System.out.println("Eligible to vote");
+  }
+}
