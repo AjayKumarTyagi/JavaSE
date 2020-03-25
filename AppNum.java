f6942627faab3b9bac383d90ee74b2f3781fa376
Ajay Kumar Tyagi <ajaytyagi0209@gmail.com>
2020-03-25 12:37:41 +0530

    triangular number checked

---
+++ AppNum.java
@@ -0,0 +1,22 @@
+public class AppNum
+{
+  int num=10;
+  public boolean isTriangular()
+  {
+    int i,sum=0;
+    for (i=0;sum<num ;i++ )
+    {
+      sum=sum+i;
+      System.out.println(sum);
+    }
+    System.out.println("\n"+i);
+    if(sum==num)
+    return true;
+    else
+    return false;
+  }
+  public static void main(String[] args) {
+    AppNum n=new AppNum();
+    System.out.println(n.isTriangular());
+  }
+}
