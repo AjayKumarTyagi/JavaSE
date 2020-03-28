bbddee13684c3224ad66c4a11932565d0d490ddb
Ajay Kumar Tyagi <ajaytyagi0209@gmail.com>
2020-03-28 21:22:57 +0530

    updated, optimised

--- Allrounder.java
+++ Allrounder.java
@@ -1,47 +1,65 @@
-class Player{
-  String name="";
+import java.lang.Object;
+
+class Player {
+  String name = "";
   int age;
   double wt;
-  public Player(){
-    name="Bosco de Gama";
-    age=5;
-    wt=12.0;
+
+  public Player() {
+    name = "Bosco de Gama";
+    age = 5;
+    wt = 12.0;
   }
 }
- class Cricket extends Player{
-  int runs,matches;
+
+class Cricket extends Player {
+  int runs, matches;
   double strike;
-  Cricket(){
+
+  Cricket() {
     super();
-    runs=999;
-    matches=10;
-    strike=999.89;
+    runs = 999;
+    matches = 10;
+    strike = 999.89;
   }
-  public void display(){
-    System.out.println("runs: "+runs);
-    System.out.println("matches: "+matches);
-    System.out.println("strike: "+strike);
+
+  public void display() {
+    System.out.println("runs: " + runs);
+    System.out.println("matches: " + matches);
+    System.out.println("strike: " + strike);
   }
 }
- class Football extends Player{
-  int goals,matches,assists;
-  Football(){
+
+class Football extends Player {
+  int goals, matches, assists;
+
+  Football() {
     super();
-    goals=999;
-    matches=9;
-    assists=0;
+    goals = 999;
+    matches = 9;
+    assists = 0;
   }
- public void display(){
-   System.out.println("goals: "+goals);
-   System.out.println("matches: "+matches);
-   System.out.println("assists: "+assists);
- }
+
+  public void display() {
+    System.out.println("goals: " + goals);
+    System.out.println("matches: " + matches);
+    System.out.println("assists: " + assists);
+  }
 }
- class Allrounder{
-public static void main(String args[]){
-  Cricket play1=new Cricket();
-  Football play2=new Football();
-  play1.display();
-  play2.display();
-}
+
+class Allrounder {
+  public void finalize() {
+    System.out.println("object is garbage collected");
+  }
+
+  public static void main(final String args[]) {
+    Cricket play1 = new Cricket();
+    Football play2 = new Football();
+    play1.display();
+    play2.display();
+    play1 = null;
+    play2 = null;
+    System.gc();
+  }
+
 }
